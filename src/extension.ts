98d50f89ea11e55ef698e593cb640a0b573767c8
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-12-07 13:09:50 -0500

    ♻️ Refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TextEncoder } from 'util';
+
+const { Configuration, OpenAIApi } = require("openai");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,34 +20,44 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 
-		vscode.window.showInformationMessage('process.env.OPENAI_API_TOKEN : ' + process.env.OPENAI_API_TOKEN);
-
-		const { Configuration, OpenAIApi } = require("openai");
 		const configuration = new Configuration({
 			apiKey: process.env.OPENAI_API_TOKEN,
 		});
 
-		let clipboardContent = await vscode.env.clipboard.readText();
 		const editor = vscode.window.activeTextEditor;
-		const selectedText = editor?.document.getText(editor.selection);
+		const selection = editor?.selection;
+		const selectedText = editor?.document.getText(selection);
 
+		vscode.window.showInformationMessage('Creating Test Case for you!');
 
-		vscode.window.showInformationMessage('Hello World from kanzen!');
-		vscode.window.showInformationMessage(clipboardContent);
-		vscode.window.showInformationMessage(selectedText + "");
+		const command = "Create JUnit5 Test Case for the following piece of code. Include positive and negative test cases. :";
 
-		const openai = new OpenAIApi(configuration);
-		const response = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: "Say this is a test",
-			max_tokens: 7,
-			temperature: 0,
-		});
+		const response = await callOpenAI(configuration, command + selectedText);
+
 		vscode.window.showInformationMessage("OpenAI Response :" + response.data.choices[0].text);
+
+		//Write the response to the file
+		const fileUri = vscode.Uri.file("/Users/rajesh/Learn/tmp/PalindromeTest.java");
+		await vscode.workspace.fs.writeFile(fileUri, new TextEncoder().encode(response.data.choices[0].text));
+		vscode.window.showTextDocument(fileUri, { preview: false });
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+async function callOpenAI(configuration: any, selectedText: any) {
+	const openai = new OpenAIApi(configuration);
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: selectedText,
+		max_tokens: 1024,
+		temperature: 1,
+	});
+	return response;
+}
+
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	vscode.window.showInformationMessage('Deactivating Kanzen Extension');
+}
